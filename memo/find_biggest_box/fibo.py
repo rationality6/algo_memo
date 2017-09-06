ca9601a8e8d2a2db068233cdb3eadb5b19525abe
Hyun <rationality6@gmail.com>
2017-09-06 22:19:47 +0900

    added fibonacci

---
+++ memo/find_biggest_box/fibo.py
@@ -0,0 +1,10 @@
+def fibo(n):
+    if n < 2:
+        print(n)
+        return n
+    else:
+        print(n)
+        return fibo(n - 1) + fibo(n - 2)
+
+
+print(fibo(10))
--- memo/find_biggest_box/mock.py
+++ memo/find_biggest_box/mock.py
@@ -27,6 +27,7 @@
 import itertools
 import numpy as np
 
+
 def findLargestSquare(board):
     a = np.array(board)
     max_size = min(a.shape)
@@ -34,14 +35,15 @@ def findLargestSquare(board):
         k = max_size - size + 1
         for i, j in itertools.product(range(k), repeat=2):
             print('checking position: ', (i, j))
-            sub_square = a[i:i+size, j:j+size]
+            sub_square = a[i:i + size, j:j + size]
             count = len(np.unique(sub_square))
             print(count, ' elements in: \n', sub_square)
             if count == 1:
                 return len(sub_square) ** 2
                 # if you also want to know the position, you could do
                 # return len(sub_square) ** 2 ,  (i, j)
-    return 1 # no squares found
+    return 1  # no squares found
+
 
 testBoard = [
     ['X', 'O', 'O', 'O', 'X'],
@@ -52,3 +54,9 @@ testBoard = [
 ]
 
 findLargestSquare(testBoard)
+
+
+import itertools
+for a, b in itertools.product(range(3), repeat=3):
+    print(a)
+    print(b)
