9e4ebfd846206959182b2725dd8095901be16686
Hyun Ahn <rationality6@gmail.com>
2017-10-22 15:17:35 +0900

    test

--- memo/memo932874.py
+++ memo/memo932874.py
@@ -81,24 +81,20 @@ print(arr0)
 print(quick_sort(arr0))
 
 
-
-
-
-
 import math
 from functools import reduce
-nu = reduce(lambda a,b:a*b,[*range(28,37)])
-result = nu/math.factorial(9)
+nu = reduce(lambda a, b: a * b, [*range(28, 37)])
+result = nu / math.factorial(9)
 print(result)
-print(len([i for i in range(1,101) if i % 3 == 0 or i % 2 == 0]))
-print(len([i for i in range(1,101) if i % 3 == 0 or i % 10 == 0]))
-print(len([i for i in range(1,101) if i % 9 == 0 or i % 4 == 0]))
-print(len([i for i in range(1,101) if i % 3 == 0 or i % 4 == 0]))
-print(len([i for i in range(1,101) if i % 5 == 0 or i % 3 == 0]))
-print(len([i for i in range(1,101) if i % 5 == 0 or i % 4 == 0]))
-print(len([i for i in range(1,101) if i % 5 == 0 or i % 8 == 0]))
-print(len([i for i in range(1,101) if i % 2 == 0 or i % 3 == 0]))
-print(len([i for i in range(1,101) if i % 10 == 0 or i % 7 == 0]))
+print(len([i for i in range(1, 101) if i % 3 == 0 or i % 2 == 0]))
+print(len([i for i in range(1, 101) if i % 3 == 0 or i % 10 == 0]))
+print(len([i for i in range(1, 101) if i % 9 == 0 or i % 4 == 0]))
+print(len([i for i in range(1, 101) if i % 3 == 0 or i % 4 == 0]))
+print(len([i for i in range(1, 101) if i % 5 == 0 or i % 3 == 0]))
+print(len([i for i in range(1, 101) if i % 5 == 0 or i % 4 == 0]))
+print(len([i for i in range(1, 101) if i % 5 == 0 or i % 8 == 0]))
+print(len([i for i in range(1, 101) if i % 3 == 0 or i % 10 == 0]))
+print(len([i for i in range(1, 101) if i % 10 == 0 or i % 7 == 0]))
 import math
 print(math.factorial(6))
 import math
@@ -106,21 +102,38 @@ a = math.factorial(6)
 print(a)
 
 from functools import reduce
-print(reduce(lambda a,b:a*b,[*range(7,10)]))
+print(reduce(lambda a, b: a * b, [*range(7, 10)]))
 
 import math
-r = math.factorial(60)/(math.factorial(56)*math.factorial(4))
+r = math.factorial(60) / (math.factorial(56) * math.factorial(4))
 print(r)
-print(1/r)
+print(1 / r)
 
 import math
-r = math.factorial(45)/(math.factorial(39)*math.factorial(6))
+r = math.factorial(45) / (math.factorial(39) * math.factorial(6))
 print(r)
-print(1/r)
+print(1 / r)
 
 from functools import reduce
-n = reduce(lambda a,b:a*b,[*range(97,103)])
+n = reduce(lambda a, b: a * b, [*range(97, 103)])
 import math
 d = math.factorial(6)
-result = n/d
+result = n / d
 print(result)
+
+def get_prime(N):
+    primes = set()
+    for i in range(2, N):
+        if all(i % z > 0 for z in primes):
+            primes.add(i)
+    return primes
+print(get_prime(30))
+
+import itertools
+s = set()
+for i in itertools.permutations("PRIOR"):
+    s.add(i)
+print(s)
+print(len(s))
+r = itertools.permutations("PRIOR")
+print(len([*r]))
